9075ba32a421d74c629ff638e8c40a136baeb8ae
Justin T <43036766+jtorres1215@users.noreply.github.com>
2019-04-15 08:31:20 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#class-name").click(function() {
+    var year = $("#year").val();
+    if(year === "2019") {
+        alert("Senior");
+    }    
+    else if (year === "2020") {
+        alert("Junior");
+}
+ else if (year === "2021") {
+        alert("Sophomore");
+}
+ else if (year === "2022") {
+        alert("Freshman");
+}
+});
